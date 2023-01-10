54d4a81c7e6059a71eb647dae31e03c22bdafcfe
JeahaOh <jeaha1217@gmail.com>
2023-01-10 22:51:30 +0900

    20230110

---
+++ short_vue_3.x/project/src/views/1_databinding/DataBindingStringView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Hello {{ userName }}</h1>
+    <p>{{ message }}</p>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      userName: 'John Doe',
+      message: '잠 온다',
+      arr: [],
+      abj: {}
+    }
+  }
+}
+</script>
+<style scoped>
+.text-red {
+  color: red;
+}
+</style>
