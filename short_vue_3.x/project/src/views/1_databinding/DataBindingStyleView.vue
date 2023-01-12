38b19ad43a31ba3c2f74d3cd60faa91180965f7e
JeahaOh <jeaha1217@gmail.com>
2023-01-12 20:38:39 +0900

    20230112

---
+++ short_vue_3.x/project/src/views/1_databinding/DataBindingStyleView.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div style="color: red; font-size: 30px">
+      스타일 바인딩 -> 색 : red, 폰트 크기 : 30px
+    </div>
+    <div :style="style1">
+      스타일 바인딩 -> 색 : {{ colorName }}, 폰트 크기 : 30px
+    </div>
+    <button @click="click">change</button>
+  </div>
+</template>
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      colorName: 'green',
+      style1: {
+        color: 'green',
+        fontSize: '30px'
+      }
+    }
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {
+    click() {
+      this.colorName = 'blue'
+      this.style1.color = 'blue'
+    }
+  }
+}
+</script>
