889d81091d2249d65fcb78994442cda738e7d446
JeahaOh <jeaha1217@gmail.com>
2023-01-26 22:27:57 +0900

    2023 01 26
    버젼 문제...

---
+++ vue.webgame/3.number-baseball/NumberBaseball.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>{{ result }}</h1>
+    <form v-on:submit="onSubmitForm">
+      <input ref="answer" maxlength="4" v-model="value" />
+      <button>GO</button>
+    </form>
+    <div>{{}} tries</div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      value: "",
+      result: "",
+    };
+  },
+  methods: {
+    onSubmitForm(e) {
+      e.preventDefault();
+    },
+  },
+};
+</script>
+<style></style>
