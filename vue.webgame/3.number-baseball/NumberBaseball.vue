eff4dc97e3746f69220c5694f20cd24362e732d7
JeahaOh <jeaha1217@gmail.com>
2023-01-27 20:43:17 +0900

    2023 01 27

--- vue.webgame/3.number-baseball/NumberBaseball.vue
+++ vue.webgame/3.number-baseball/NumberBaseball.vue
@@ -1,24 +1,36 @@
 <template>
   <div>
     <h1>{{ result }}</h1>
-    <form v-on:submit="onSubmitForm">
-      <input ref="answer" maxlength="4" v-model="value" />
-      <button>GO</button>
+    <form @submit.prevent="onSubmitForm">
+      <input ref="answer" minlength="4" maxlength="4" v-model="value" />
+      <button type="submit">GO</button>
     </form>
-    <div>{{}} tries</div>
+    <div>{{ tries.length }} tries</div>
+    <ul>
+      <li v-for="tri in tries">
+        <div>{{ tri.try }}</div>
+        <div>{{ tri.result }}</div>
+      </li>
+    </ul>
   </div>
 </template>
 <script>
 export default {
   data() {
     return {
+      tries: [],
       value: "",
       result: "",
     };
   },
   methods: {
     onSubmitForm(e) {
-      e.preventDefault();
+      this.tries.push({
+        try: this.value,
+        result: "HOME RUN",
+      });
+      this.value = "";
+      this.$refs.answer.focus();
     },
   },
 };
