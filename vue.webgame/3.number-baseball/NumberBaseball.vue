999264dd5e2ba82fab9844eabfe62b78eac6a023
JeahaOh <jeaha1217@gmail.com>
2023-01-28 21:05:08 +0900

    2023 01 28

--- vue.webgame/3.number-baseball/NumberBaseball.vue
+++ vue.webgame/3.number-baseball/NumberBaseball.vue
@@ -15,9 +15,23 @@
   </div>
 </template>
 <script>
+/**
+ * Radom하게 4개의 숫자 뽑기
+ */
+const getNumbers = () => {
+  const candidates = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const array = [];
+  for (let i = 0; i < 4; i++) {
+    const chosen = candidates.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
+    array.push(chosen);
+  }
+  return array;
+};
+
 export default {
   data() {
     return {
+      answer: getNumbers(),
       tries: [],
       value: "",
       result: "",
@@ -25,12 +39,51 @@ export default {
   },
   methods: {
     onSubmitForm(e) {
-      this.tries.push({
-        try: this.value,
-        result: "HOME RUN",
-      });
-      this.value = "";
-      this.$refs.answer.focus();
+      if (this.value === this.answer.join("")) {
+        // 정답인 경우
+        this.tries.push({
+          try: this.value,
+          result: "HOME RUN!!",
+        });
+        this.result = "HOME RUN!!";
+        this.answer = getNumbers();
+        alert("게임을 다시 실행합니다.");
+
+        this.tries = [];
+        this.value = "";
+        this.$refs.answer.focus();
+      } else {
+        // 틀린 경우
+        if (this.tries.length >= 9) {
+          this.result = `GAME END!!\nthe answer was ${this.answer.join(",")}!`;
+          alert("게임을 다시 실행합니다.");
+          this.value = "";
+          this.answer = getNumbers();
+          this.tries = [];
+          this.value = "";
+          this.$refs.answer.focus();
+        }
+        let strike = 0;
+        let ball = 0;
+
+        const answerArray = this.value.split("").map((v) => parseInt(v));
+        for (let i = 0; i < 4; i++) {
+          if (answerArray[i] === this.answer[i]) {
+            // 숫자, 자릿수 모두 정답
+            strike++;
+          } else if (this.answer.includes(answerArray[i])) {
+            // 숫자만 정답
+            ball++;
+          }
+        }
+
+        this.tries.push({
+          try: this.value,
+          result: `${strike} strike, ${ball} ball.`,
+        });
+        this.value = "";
+        this.$refs.answer.focus();
+      }
     },
   },
 };
