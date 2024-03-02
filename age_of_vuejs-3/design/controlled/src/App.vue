09150cb09a2ec3259eade6e7ead3c665abd60f90
JeahaOh <jeaha1217@gmail.com>
2024-03-02 12:25:54 +0900

    vue component design pattern

---
+++ age_of_vuejs-3/design/controlled/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <check-box></check-box>
+</template>
+
+<script>
+import CheckBox from './components/CheckBox.vue';
+
+export default {
+  components: {
+    CheckBox,
+  },
+  created() {},
+};
+</script>
