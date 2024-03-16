37e95fd01033b1c90296068a10025ffbc1425d91
JeahaOh <jeaha1217@gmail.com>
2024-03-16 16:08:37 +0900

    watch API

--- vue-composition/vue3-todo/src/components/TodoList.vue
+++ vue-composition/vue3-todo/src/components/TodoList.vue
@@ -9,12 +9,18 @@
   </div>
 </template>
 <script>
+import { watch } from 'vue';
 export default {
   props: ['todoItems'],
   setup(props, context) {
     const removeTodo = (item, idx) => {
       context.emit('remove', item, idx);
     };
+
+    // watch는 데이터의 관리가 어렵고, 리소스 사용이 많으니 지양하는 것이 좋음.
+    watch(props.todoItems, (newValue) => {
+      console.log(newValue);
+    });
 
     return { removeTodo };
   },
