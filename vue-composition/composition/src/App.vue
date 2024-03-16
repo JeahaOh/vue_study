af8cc8fe96ce5ce4bf4a3055262a5b19b0b046e8
JeahaOh <jeaha1217@gmail.com>
2024-03-16 14:19:19 +0900

    Composition API 기본 코드 작성

---
+++ vue-composition/composition/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <p>{{ message }}</p>
+    <button @click="changeMessage">Change Message</button>
+  </div>
+</template>
+<script>
+import { ref } from 'vue';
+export default {
+  // data() {
+  //   return {
+  //     message: 'hell',
+  //   };
+  // },
+  // methods: {
+  //   changeMessage() {
+  //     this.message = 'hell no';
+  //   },
+  // },
+  setup() {
+    const message = ref('hell');
+
+    const changeMessage = () => {
+      message.value = 'hell no';
+    };
+    return { message, changeMessage };
+  },
+};
+</script>
