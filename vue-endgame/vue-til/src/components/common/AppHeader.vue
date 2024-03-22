b92a3f56e1c8878810b1c2f785f8a92e45f96ab0
JeahaOh <jeaha1217@gmail.com>
2024-03-23 08:53:51 +0900

    .

---
+++ vue-endgame/vue-til/src/components/common/AppHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    header
+  </div>
+</template>
+<script>
+export default {
+  components: {},
+  data() {
+    return {}
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {}
+}
+</script>
